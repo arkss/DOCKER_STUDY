bce36431f148509a3bd12dd1cb0a4d4a683157a9
arkss <rkdalstjd9@naver.com>
2021-09-08 00:59:43 +0900

    9장

---
+++ Minsung/section09_10/docker-fullstack-app/backend/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const db = require("./db");
+
+const app = express();
+
+// app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.get('/api/values', function(req, res){
+    db.pool.query('SELECT * FROM lists;',
+        (err, results, fields) => {
+            if (err) 
+                return res.status(500).send(err)
+            else 
+                return res.json(results)
+        })
+})
+
+app.post('/api/value', function(req, res, next){
+    db.pool.query(`INSERT INTO lists (value) VALUES("${req.body.value}")`,
+        (err, results, fields) => {
+            if (err) 
+                return res.status(500).send(err)
+            else 
+                return res.json({ success: true, value: req.body.value })
+        })
+})
+
+app.listen(5000, ()=>{
+    console.log("5000 포트에서 시작")
+})
